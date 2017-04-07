a99cee37a604f878285ce3d285e24ac430685ad3
Ivan Minchev <vanko@blsdev0595lax.blackline.corp>
2017-04-07 00:10:30 -0700

    Something...

---
+++ src/Sisyphus.Host.Console/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Sisyphus.Host.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World!");
+        }
+    }
+}
